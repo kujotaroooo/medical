36df021c498831a59ce344940f9d1d630f5ccf06
ROHAN KISHIBE <126190522+kujotaroooo@users.noreply.github.com>
2026-02-03 14:28:13 +0800

    db commit

--- app/view/login.php
+++ app/view/login.php
@@ -15,20 +15,12 @@
 
 
         
-        <link rel="stylesheet" type="text/css" href="../css/style.css?id=C3B723FC2F89073B3811813EDE024F6A64AABFCBBB3D81F9A985558AB00B8FC3" /><style>.illustrationClass {background-image:url(/adfs/portal/illustration/illustration.jpg?id=D15CF3C7F4E3571428A79296B0280DCA21E3F9626F2CAD1D96615C4F8D8913B1);}</style>
+        <link rel="stylesheet" type="text/css" href="/medical/public/css/style.css?id=C3B723FC2F89073B3811813EDE024F6A64AABFCBBB3D81F9A985558AB00B8FC3" /><style>.illustrationClass {background-image:url(/adfs/portal/illustration/illustration.jpg?id=D15CF3C7F4E3571428A79296B0280DCA21E3F9626F2CAD1D96615C4F8D8913B1);}</style>
 
     </head>
-	<style>
-		body{
- 
- 		}
-	</style>
+
     <body dir="ltr" class="body">
-    <div id="noScript" style="position:static; width:100%; height:100%; z-index:100">
-        <h1>JavaScript required</h1>
-        <p>JavaScript is required. This web browser does not support JavaScript or JavaScript in this web browser is not enabled.</p>
-        <p>To find out if your web browser supports JavaScript or to enable JavaScript, see web browser help.</p>
-    </div>
+   
     <script type="text/javascript" language="JavaScript">
          document.getElementById("noScript").style.display = "none";
     </script>
@@ -39,7 +31,7 @@
         <div id="contentWrapper" class="float">
             <div id="content">
                 <div id="header">
-                    <img class='logoImage' id='companyLogo' src='../image/med.png' alt='Alang-Alang State University'/>
+                    <img class='logoImage' id='companyLogo' src='/medical/public/images/med.png' alt='Alang-Alang State University'/>
                 </div>
                 <main>
                     <div id="workArea">
@@ -51,7 +43,7 @@
     <div id="loginArea">        
         <div id="loginMessage" class="groupMargin">Log in</div>
 
-        <form method="post" id="loginForm" autocomplete="off" novalidate="novalidate" onKeyPress="if (event && event.keyCode == 13) Login.submitLoginRequest();" action="/adfs/ls/?SAMLRequest=hVJRb4IwGPwrpO8Iw4HaAImAJiZuMbrtYS9Lg5%2BxSWlZv5bpv1%2FBbNMX99T0etfv7toUWSNaOrfmKLfwaQGNd2qERDocZMRqSRVDjlSyBpCamu7mT2sajULaamVUrQS5ktxXMETQhitJvFWVkY%2BiDJNZOYvLIp6Ok4dwuigmkyKppsvZY7yoIuK9gUbHz4iTOxGihZVEw6RxUBglfhj54fgljOg4oVH8TrzKZeCSmUF1NKZFGgQHHHVM7JVTjmBvA7Y%2FYCAwIF6pJEJ%2F2z3f9YVEa6u1W33etILX3BBvqXQNQ30ZOTCB0JvcuJy8g19k%2FhO7H2Yb0DvQHa%2Fhdbv%2Bs9icO7S3LvtCg9Ztt4Btb4HkaY%2FRoQed35GyFk5pcE1OL0%2F97MKtqo1y%2Fs%2B9%2B4b9k71H%2BN4%2FDFRqNJPIXQculRDqq9TAjEtqtAUS5JeRtx8q%2FwY%3D&RelayState=https%3A%2F%2Fmyvsu.valdosta.edu%2Fapex%2Ff%3Fp%3D100&client-request-id=42e5fd2f-6a27-451e-b34c-0380000000e7" >
+        <form method="post" id="loginForm" autocomplete="off" novalidate="novalidate" action="/medical/app/controller/loginAuth.php" >
             <div id="error" class="fieldMargin error smallText">
                 <span id="errorText" for="" aria-live="assertive" role="alert"></span>
             </div>
@@ -77,203 +69,7 @@
                 </div>
             </div>
             <input id="optionForms" type="hidden" name="AuthMethod" value="FormsAuthentication"/>
-        </form>
-
-             <div id="authOptions">
-         <form id="options" method="post" action="https://fs.valdosta.edu:443/adfs/ls/?SAMLRequest=hVJRb4IwGPwrpO8Iw4HaAImAJiZuMbrtYS9Lg5%2BxSWlZv5bpv1%2FBbNMX99T0etfv7toUWSNaOrfmKLfwaQGNd2qERDocZMRqSRVDjlSyBpCamu7mT2sajULaamVUrQS5ktxXMETQhitJvFWVkY%2BiDJNZOYvLIp6Ok4dwuigmkyKppsvZY7yoIuK9gUbHz4iTOxGihZVEw6RxUBglfhj54fgljOg4oVH8TrzKZeCSmUF1NKZFGgQHHHVM7JVTjmBvA7Y%2FYCAwIF6pJEJ%2F2z3f9YVEa6u1W33etILX3BBvqXQNQ30ZOTCB0JvcuJy8g19k%2FhO7H2Yb0DvQHa%2Fhdbv%2Bs9icO7S3LvtCg9Ztt4Btb4HkaY%2FRoQed35GyFk5pcE1OL0%2F97MKtqo1y%2Fs%2B9%2B4b9k71H%2BN4%2FDFRqNJPIXQculRDqq9TAjEtqtAUS5JeRtx8q%2FwY%3D&RelayState=https%3A%2F%2Fmyvsu.valdosta.edu%2Fapex%2Ff%3Fp%3D100&client-request-id=42e5fd2f-6a27-451e-b34c-0380000000e7">
-             <script type="text/javascript">
-                function SelectOption(option) {
-                    var w = document.getElementById('waitingWheelDiv');
-                    if(w) w.style.display = 'inline';
-                    var i = document.getElementById('optionSelection');
-                    i.value = option;
-                    document.forms['options'].submit();
-                    return false;
-                }
-             </script>
-             <input id="optionSelection" type="hidden" name="AuthMethod" />
-             <input id="userNameInputOptionsHolder" name="UserName" value="" type="hidden"/>
-             <div id='authOptionLinks' class='groupMargin'><div id="waitingWheelDiv" style="display: none;"><div id="WaitingWheel">
-    <!-- NOTE: This style portion is identical to cookie pull page, they are not in shared css file because of legacy dependancies for custom themes-->
-    <!-- CSS for small "waiting" wheel -->
-    <style>
-        #floatingCirclesG {
-            position: relative;
-            width: 125px;
-            height: 125px;
-            margin: auto;
-            transform: scale(0.4);
-            -o-transform: scale(0.4);
-            -ms-transform: scale(0.4);
-            -webkit-transform: scale(0.4);
-            -moz-transform: scale(0.4);
-        }
-
-        .f_circleG {
-            position: absolute;
-            height: 22px;
-            width: 22px;
-            border-radius: 12px;
-            -o-border-radius: 12px;
-            -ms-border-radius: 12px;
-            -webkit-border-radius: 12px;
-            -moz-border-radius: 12px;
-            animation-name: f_fadeG;
-            -o-animation-name: f_fadeG;
-            -ms-animation-name: f_fadeG;
-            -webkit-animation-name: f_fadeG;
-            -moz-animation-name: f_fadeG;
-            animation-duration: 1.2s;
-            -o-animation-duration: 1.2s;
-            -ms-animation-duration: 1.2s;
-            -webkit-animation-duration: 1.2s;
-            -moz-animation-duration: 1.2s;
-            animation-iteration-count: infinite;
-            -o-animation-iteration-count: infinite;
-            -ms-animation-iteration-count: infinite;
-            -webkit-animation-iteration-count: infinite;
-            -moz-animation-iteration-count: infinite;
-            animation-direction: normal;
-            -o-animation-direction: normal;
-            -ms-animation-direction: normal;
-            -webkit-animation-direction: normal;
-            -moz-animation-direction: normal;
-        }
-
-        #frotateG_01 {
-            left: 0;
-            top: 51px;
-            animation-delay: 0.45s;
-            -o-animation-delay: 0.45s;
-            -ms-animation-delay: 0.45s;
-            -webkit-animation-delay: 0.45s;
-            -moz-animation-delay: 0.45s;
-        }
-
-        #frotateG_02 {
-            left: 15px;
-            top: 15px;
-            animation-delay: 0.6s;
-            -o-animation-delay: 0.6s;
-            -ms-animation-delay: 0.6s;
-            -webkit-animation-delay: 0.6s;
-            -moz-animation-delay: 0.6s;
-        }
-
-        #frotateG_03 {
-            left: 51px;
-            top: 0;
-            animation-delay: 0.75s;
-            -o-animation-delay: 0.75s;
-            -ms-animation-delay: 0.75s;
-            -webkit-animation-delay: 0.75s;
-            -moz-animation-delay: 0.75s;
-        }
-
-        #frotateG_04 {
-            right: 15px;
-            top: 15px;
-            animation-delay: 0.9s;
-            -o-animation-delay: 0.9s;
-            -ms-animation-delay: 0.9s;
-            -webkit-animation-delay: 0.9s;
-            -moz-animation-delay: 0.9s;
-        }
-
-        #frotateG_05 {
-            right: 0;
-            top: 51px;
-            animation-delay: 1.05s;
-            -o-animation-delay: 1.05s;
-            -ms-animation-delay: 1.05s;
-            -webkit-animation-delay: 1.05s;
-            -moz-animation-delay: 1.05s;
-        }
-
-        #frotateG_06 {
-            right: 15px;
-            bottom: 15px;
-            animation-delay: 1.2s;
-            -o-animation-delay: 1.2s;
-            -ms-animation-delay: 1.2s;
-            -webkit-animation-delay: 1.2s;
-            -moz-animation-delay: 1.2s;
-        }
-
-        #frotateG_07 {
-            left: 51px;
-            bottom: 0;
-            animation-delay: 1.35s;
-            -o-animation-delay: 1.35s;
-            -ms-animation-delay: 1.35s;
-            -webkit-animation-delay: 1.35s;
-            -moz-animation-delay: 1.35s;
-        }
-
-        #frotateG_08 {
-            left: 15px;
-            bottom: 15px;
-            animation-delay: 1.5s;
-            -o-animation-delay: 1.5s;
-            -ms-animation-delay: 1.5s;
-            -webkit-animation-delay: 1.5s;
-            -moz-animation-delay: 1.5s;
-        }
-
-        @keyframes f_fadeG {
-            0% {
-                background-color: rgb(47, 146, 212);
-            }
-
-            100% {
-                background-color: rgb(255, 255, 255);
-            }
-        }
-
-        @-o-keyframes f_fadeG {
-            0% {
-                background-color: rgb(47, 146, 212);
-            }
-
-            100% {
-                background-color: rgb(255, 255, 255);
-            }
-        }
-
-        @-ms-keyframes f_fadeG {
-            0% {
-                background-color: rgb(47, 146, 212);
-            }
-
-            100% {
-                background-color: rgb(255, 255, 255);
-            }
-        }
-
-        @-webkit-keyframes f_fadeG {
-            0% {
-                background-color: rgb(47, 146, 212);
-            }
-
-            100% {
-                background-color: rgb(255, 255, 255);
-            }
-        }
-
-        @-moz-keyframes f_fadeG {
-            0% {
-                background-color: rgb(47, 146, 212);
-            }
-
-            100% {
-                background-color: rgb(255, 255, 255);
-            }
-        }
-    </style>
-
-   
-</div></div></div>
-         </form>
+    
       </div>
 
        
@@ -288,9 +84,7 @@
                 <div id="footerPlaceholder"></div>
             </div>
             <footer id="footer">
-                <div id="footerLinks" class="floatReverse">
-                    <div><span id="copyright">&#169; 2024 Alang Alang State University</span><a id="# " class="pageLink footerLink " href="">Home</a><a id="" class="pageLink footerLink" href="">Helpdesk</a></div>
-                </div>
+               
             </footer>
         </div>     
     </div>
---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$localhost = "localhost";
+$username = "root";
+$password = "";
+$database = "medical";
+
+$conn = new mysqli($localhost, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
