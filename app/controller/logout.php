b1d84c8570fcbe2a953e9d6356b81dab6b97ef53
ROHAN KISHIBE <126190522+kujotaroooo@users.noreply.github.com>
2026-02-05 21:25:42 +0800

    comments removed

--- app/controller/logout.php
+++ app/controller/logout.php
@@ -3,9 +3,9 @@
 session_start();
 if($_SERVER['REQUEST_METHOD']=='POST'){
   if(isset($_POST['logout'])){
-    session_destroy(); // destroy session
+    session_destroy(); 
     header("location:../view/login.php"); 
-    exit(); // MUST add exit after header
+    exit(); 
   }
 }
 ?>
