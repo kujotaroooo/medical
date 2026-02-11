e7ee8f14aca20c3bda7a4de0c45f89f975c6d53a
ROHAN KISHIBE <126190522+kujotaroooo@users.noreply.github.com>
2026-02-11 16:06:57 +0800

    db commit

--- app/view/dashboard.php
+++ app/view/dashboard.php
@@ -1,11 +1,9 @@
 <?php
 session_start();
-if (!isset($_SESSION['user']) ) {
-      header("Location: 404.php");
-    
+if (!isset($_SESSION['user'])) {
+    header("Location: 404.php");
     exit();
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,15 +11,43 @@ if (!isset($_SESSION['user']) ) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="/medical/public/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/medical/public/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+    <link href="/medical/public/css/sb-admin-2.css" rel="stylesheet">
 </head>
 <body>
-    <h1>DASHBOARD</h1>
-    <h2>Welcome, <?php echo $_SESSION['user']['username']; ?>!</h2>
-<form method="post" action="../controller/logout.php">
-    <input type="submit" name="logout" value="Logout">
-</form>
+    <div id="wrapper">
+        <?php include('leftbar.php'); ?>
 
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h4 class="page-header">
+                        <?php echo strtoupper("welcome " . htmlentities($_SESSION['user']['username'])); ?>
+                    </h4>
+                </div>
+            </div>
 
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Dashboard</div>
+                        <div class="panel-body">
+                            <?php echo $_SESSION['user']['username']; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <form method="post" action="../controller/logout.php">
+                <input type="submit" name="logout" value="Logout">
+            </form>
+        </div>
+    </div>
+
+    <script src="/medical/public/bower_components/jquery/dist/jquery.min.js" type="text/javascript"></script>
+    <script src="/medical/public/bower_components/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="/medical/public/bower_components/metisMenu/dist/metisMenu.min.js" type="text/javascript"></script>
+    <script src="/medical/public/js/sb-admin-2.js" type="text/javascript"></script>
 </body>
 </html>
-
---
+++ app/view/leftbar.php
@@ -0,0 +1,107 @@
+
+<link href="/medical/public/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<nav class="navbar navbar-default navbar-static-top" role="navigation"
+			style="margin-bottom: 0">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse"
+					data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span> <span
+						class="icon-bar"></span> <span class="icon-bar"></span> <span
+						class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="index.html">Clinic Records VSUA</a>
+			</div>
+						 <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        
+                        <li>
+                   <a href="dashboard.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        </li>
+                        <li>
+    <a href="#"><i class="fa fa-file fa-fw"></i> Medical Records <span class="fa arrow"></span></a>
+    <ul class="nav nav-second-level">
+        
+        <li>
+            <a href="#">BEED <span class="fa arrow"></span></a>
+            <ul class="nav nav-third-level">
+                <li><a href="add-course.php?course=BEED&year=1">2026</a></li>
+                
+            </ul>
+        </li>
+           <li>
+            <a href="#">BSIT <span class="fa arrow"></span></a>
+            <ul class="nav nav-third-level">
+                <li><a href="add-course.php?course=BEED&year=1">1st Year</a></li>
+                <li><a href="add-course.php?course=BEED&year=2">2nd Year</a></li>
+                <li><a href="add-course.php?course=BEED&year=3">3rd Year</a></li>
+                <li><a href="add-course.php?course=BEED&year=4">4th Year</a></li>
+            </ul>
+        </li>
+              <li>
+            <a href="#">BSIT <span class="fa arrow"></span></a>
+            <ul class="nav nav-third-level">
+                <li><a href="add-course.php?course=BSIT&year=1">1st Year</a></li>
+                <li><a href="add-course.php?course=BSIT&year=2">2nd Year</a></li>
+                <li><a href="add-course.php?course=BSIT&year=3">3rd Year</a></li>
+                <li><a href="add-course.php?course=BSIT&year=4">4th Year</a></li>
+            </ul>
+        </li>
+              <li>
+            <a href="#">BSMATH <span class="fa arrow"></span></a>
+            <ul class="nav nav-third-level">
+                <li><a href="add-course.php?course=BSMATH&year=1">1st Year</a></li>
+                <li><a href="add-course.php?course=BSMATH&year=2">2nd Year</a></li>
+                <li><a href="add-course.php?course=BSMATH&year=3">3rd Year</a></li>
+                <li><a href="add-course.php?course=BSMATH&year=4">4th Year</a></li>
+            </ul>
+        </li>
+
+        <li><a href="add-course.php?course=BSIT">Add Program</a></li>
+   
+    </ul>
+</li>
+
+                
+                 <li>
+                            <a href="#"><i class="fa fa-user fa-fw"></i>Student<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="add-student.php">Add Student</a>
+                                </li>
+                                <li>
+                                    <a href="manage-students.php">View</a>
+                                </li>
+                            </ul>
+                           
+                        </li>
+                        
+                   <li>
+                            <a href="register.php"><i class="fa fa-files-o fa-fw"></i>Regsiter</a>
+                  </li>     
+                                            
+                   <li>
+                            <a href="manage-students.php"><i class="fa fa-users fa-fw"></i>View Students</a>
+                  </li>      
+                   <li>
+                            <a href="session.php"><i class="fa fa-calendar fa-fw"></i>Session</a>
+                  </li>
+      
+                        <li>
+                   <a href="change-password.php"><i class="fa fa-cog fa-fw"></i> Change Password</a>
+                        </li>
+                 <li>
+                   <a href="admin-profile.php"><i class="fa fa-user fa-fw"></i>Admin Profile</a>
+                        </li>
+
+
+                   <li>
+                            <a href="logout.php"><i class="fa fa-sign-out fa-fw"></i>Logout</a>
+                  </li>
+                  </ul>      				  
+                </div>
+               
+            </div>
+            
+        </nav>
+   
